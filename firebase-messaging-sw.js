95963b2694641025062850535e7a44cf5a18daa9
Ismael Abdulhakim <Ismaelabdulhakim54@gmail.com>
2026-03-06 16:21:26 +0300

    add firebase service worker

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,27 +1,19 @@
-// firebase-messaging-sw.js
-
-importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyB8bxgR6veYlGd21NOUq-is1iqaDFdMANA",
-  authDomain: "abdu-all.firebaseapp.com",
-  projectId: "abdu-all",
-  storageBucket: "abdu-all.firebasestorage.app",
-  messagingSenderId: "823333682925",
-  appId: "1:823333682925:web:8ba6bdae41fefb1c6cd6e4"
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 });
 
 const messaging = firebase.messaging();
 
-// Background notification handler
 messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
+  self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    icon: '/icon-192.png' // replace with your icon
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+    icon: "/icon.png"
+  });
 });
